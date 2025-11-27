7e4f68bf734d0806a86418464b0db5cbbebba94f
David Pérez Zuniga <davidhumbertop503@gmail.com>
2025-11-26 20:36:57 -0600

    Feat: Implement redirect endpoint to original URL

--- routers/links.py
+++ routers/links.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from config.db import get_session
 from schemas.link_schema import LinkCreate
 from models.url_model import Link
 from services.keygen import create_random_key
+from sqlmodel import select
+from fastapi.responses import RedirectResponse
 
 router = APIRouter()
 
@@ -23,3 +25,16 @@ async def link_create(link_data: LinkCreate,
     await session.commit()
     await session.refresh(new_link)
     return new_link
+
+@router.get("/{short_code}")
+async def get_link_by_short_code(short_code: str,
+                                 session: AsyncSession = Depends(get_session)):
+
+    statement = select(Link).where(Link.short_code == short_code)
+    result = await session.execute(statement)
+    link_found = result.scalars().first()
+
+    if link_found is None:
+        raise HTTPException(status_code=404, detail="Link Not Found")
+
+    return RedirectResponse(link_found.original_url)
