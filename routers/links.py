b339480b25a31ca265a2d3144510f58aaf1159fb
David Pérez Zuniga <davidhumbertop503@gmail.com>
2025-11-27 19:41:38 -0600

    Feat: Add statistics endpoint to view link metrics

--- routers/links.py
+++ routers/links.py
@@ -41,3 +41,16 @@ async def get_link_by_short_code(short_code: str,
     session.add(link_found)
     await session.commit()
     return RedirectResponse(link_found.original_url)
+
+@router.get("/{short_code}/stats")
+async def get_links_stats(short_code: str,
+                          session: AsyncSession = Depends(get_session)):
+
+    statement = select(Link).where(Link.short_code == short_code)
+    result = await session.execute(statement)
+    link_found = result.scalars().first()
+
+    if link_found is None:
+        raise HTTPException(status_code=404, detail="Link Not Found")
+
+    return link_found
